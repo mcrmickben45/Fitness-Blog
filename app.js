2b4d8583831a307fa40a9e54cc9bbc9a7385c59a
Ben McCormick <mr.mccormick@Bens-MBP.lan>
2023-11-20 19:30:15 -0800

    Added 'Get Started' modal & functionality

--- app.js
+++ app.js
@@ -1,14 +1,90 @@
-const express = require('express');
- 
-const app = express();
- 
-app.get('/', (req, res) => { 
-    res.send('A simple Node App is '
-        + 'running on this server') 
-    res.end() 
-}) 
- 
-const PORT = process.env.PORT ||5000;
- 
 app.listen(PORT,console.log(
   `Server started on port ${PORT}`));
+
+const express = require('express');
+const bodyParser = require('body-parser');
+const bcrypt = require('bcryptjs');
+const session = require('express-session');
+
+const app = express();
+const PORT = 3000;
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(session({ secret: 'your-secret-key', resave: true, saveUninitialized: true }));
+
+const users = [];
+
+const requireLogin = (req, res, next) => {
+  if (!req.session.userId) {
+    return res.redirect('/login');
+  }
+  next();
+};
+
+app.get('/', requireLogin, (req, res) => {
+  res.send(`Welcome, ${req.session.username}! <a href="/logout">Logout</a>`);
+});
+
+app.get('/register', (req, res) => {
+  res.send(`
+    <form method="post" action="/register">
+      <input type="text" name="username" placeholder="Username" required>
+      <input type="text" name="screenName" placeholder="Screen Name" required>
+      <input type="password" name="password" placeholder="Password" required>
+      <button type="submit">Register</button>
+    </form>
+  `);
+});
+
+app.post('/register', async (req, res) => {
+  const { username, screenName, password } = req.body;
+
+  if (users.some(user => user.username === username)) {
+    return res.send('Username already taken. Choose another one.');
+  }
+
+  const hashedPassword = await bcrypt.hash(password, 10);
+
+  users.push({ username, screenName, password: hashedPassword });
+
+  req.session.userId = users.length;
+  req.session.username = username;
+  req.session.screenName = screenName;
+
+  res.redirect('/');
+});
+
+app.get('/login', (req, res) => {
+  res.send(`
+    <form method="post" action="/login">
+      <input type="text" name="username" placeholder="Username" required>
+      <input type="password" name="password" placeholder="Password" required>
+      <button type="submit">Login</button>
+    </form>
+  `);
+});
+
+app.post('/login', async (req, res) => {
+  const { username, password } = req.body;
+
+  const user = users.find(user => user.username === username);
+
+  if (user && await bcrypt.compare(password, user.password)) {
+    req.session.userId = user.id;
+    req.session.username = user.username;
+    req.session.screenName = user.screenName;
+
+    return res.redirect('/');
+  }
+
+  res.send('Invalid username or password.');
+});
+
+app.get('/logout', (req, res) => {
+  req.session.destroy();
+  res.redirect('/login');
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
--- assets/script.js
+++ assets/script.js
@@ -1,23 +1,16 @@
-// Toggle navigation menu
 function toggleMenu() {
     const navLinks = document.querySelector('.nav-links');
     navLinks.classList.toggle('show');
 }
-// Show specific section
 function showSection(sectionId) {
-    // Hide all sections
     const sections = document.querySelectorAll('.content-section');
     sections.forEach(section => {
         section.style.display = 'none';
     });
-    // Show the selected section
     const selectedSection = document.getElementById(sectionId);
-    // Check if the element exists before trying to manipulate it
     if (selectedSection) {
         selectedSection.style.display = 'block';
-        // Update the app title
         const appTitle = document.getElementById('appTitle');
-        // Check if the element exists before trying to manipulate it
         if (appTitle) {
             appTitle.innerText = `No Limits - ${sectionId.charAt(0).toUpperCase() + sectionId.slice(1)}`;
         } else {
@@ -27,14 +20,12 @@ function showSection(sectionId) {
         console.error(`Section element with ID ${sectionId} not found.`);
     }
 }
-// Modal functions
 function openModal(title, content, duration, difficulty) {
     const modal = document.getElementById('modal');
     const modalTitle = document.getElementById('modalTitle');
     const modalContent = document.getElementById('modalContent');
     const modalDuration = document.getElementById('modalDuration');
     const modalDifficulty = document.getElementById('modalDifficulty');
-    // Check if the element exists before trying to manipulate it
     if (modalTitle && modalContent && modalDuration && modalDifficulty) {
         modalTitle.innerText = title;
         modalContent.innerText = content;
@@ -53,18 +44,108 @@ function closeModal() {
         console.error('Modal element not found.');
     }
 }
-// Close modal if the user clicks outside of it
 window.onclick = function (event) {
     const modal = document.getElementById('modal');
     if (modal && event.target == modal) {
         modal.style.display = 'none';
     }
 };
-// Show the home section by default
 showSection('home');
-// Additional event listener to ensure script runs after the DOM is fully loaded
 document.addEventListener("DOMContentLoaded", function() {
-    // Your existing JavaScript code
-    // For example:
     showSection('home');
 });
+const authModal = document.getElementById('modal');
+const loginForm = document.getElementById('loginForm');
+const registerForm = document.getElementById('registerForm');
+const getStartedModal = document.getElementById('getStartedModal');
+
+function showLoginForm() {
+    loginForm.style.display = 'block';
+    registerForm.style.display = 'none';
+}
+
+function showRegisterForm() {
+    loginForm.style.display = 'none';
+    registerForm.style.display = 'block';
+}
+
+window.showAuthModal = function () {
+    authModal.style.display = 'block';
+    showLoginForm(); 
+};
+
+window.showGetStartedModal = function () {
+    getStartedModal.style.display = 'block';
+};
+
+window.onclick = function (event) {
+    if (event.target === authModal || event.target === getStartedModal || event.target.classList.contains('close')) {
+        authModal.style.display = 'none';
+        getStartedModal.style.display = 'none';
+    }
+};
+
+document.getElementById('switchToRegister').addEventListener('click', showRegisterForm);
+document.getElementById('switchToLogin').addEventListener('click', showLoginForm);
+
+document.addEventListener("DOMContentLoaded", function () {
+    showSection('home');
+});
+
+window.closeModal = function () {
+document.getElementById('modal').style.display = 'none';
+document.getElementById('getStartedModal').style.display = 'none';
+};
+
+function submitLoginForm() {
+console.log("Login form submitted"); 
+window.closeModal();
+}
+
+function submitRegistrationForm() {
+console.log("Registration form submitted"); 
+window.closeModal();
+}
+
+function showGetStartedModal() {
+    const getStartedModal = document.getElementById('getStartedModal');
+    if (getStartedModal) {
+        getStartedModal.style.display = 'block';
+        showLogin(); 
+    } else {
+        console.error('GetStartedModal element not found.');
+    }
+}
+
+function closeGetStartedModal() {
+    const getStartedModal = document.getElementById('getStartedModal');
+    if (getStartedModal) {
+        getStartedModal.style.display = 'none';
+    } else {
+        console.error('GetStartedModal element not found.');
+    }
+}
+
+function showLogin() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    if (loginForm && registerForm) {
+        loginForm.style.display = 'block';
+        registerForm.style.display = 'none';
+        document.getElementById('getStartedModalTitle').innerText = 'Login';
+    } else {
+        console.error('Login or Register form elements not found.');
+    }
+}
+
+function showRegister() {
+    const loginForm = document.getElementById('loginForm');
+    const registerForm = document.getElementById('registerForm');
+    if (loginForm && registerForm) {
+        loginForm.style.display = 'none';
+        registerForm.style.display = 'block';
+        document.getElementById('getStartedModalTitle').innerText = 'Register';
+    } else {
+        console.error('Login or Register form elements not found.');
+    }
+}
